05f84779b9b456318ebd79addb98b972e11f0e50
tim neutkens <tim@weprovide.com>
2017-01-15 11:52:32 +0100

    Add requested changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { NOW_LOGS_SECRET_KEY, NOW_LOGS_PORT = 3000 } = process.env;
 
-if (process.env.NOW_LOGS_SECRET_KEY) {
-    require('now-logs')(process.env.NOW_LOGS_SECRET_KEY);
+if (NOW_LOGS_SECRET_KEY) {
+    require('now-logs')(NOW_LOGS_SECRET_KEY);
 }
 
 const micro = require('micro');
