57bda328600fff18f50caff539d498795d7c2544
Tal Bereznitskey <berzniz@gmail.com>
2017-01-22 23:25:48 +0200

    Coding style alignment + remove website component

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-module.exports = function setupServer (io) {
-  io.on('connection', (socket) => {
-      console.log('Connection. Number of connected sockets', io.engine.clientsCount);
-      socket.on('disconnect', () => {
-          console.log('Disconnect. Number of connected sockets', io.engine.clientsCount);
-      })
-      socket.on('room', (room) => {
-          socket.join(room);
-      })
-      socket.on('emit-room', (room, text) => {
-          io.to(room).emit('message', text);
-      })
-  })
+module.exports = function setupServer(io) {
+    io.on('connection', (socket) => {
+        console.log('Connection. Number of connected sockets', io.engine.clientsCount);
+        socket.on('disconnect', () => {
+            console.log('Disconnect. Number of connected sockets', io.engine.clientsCount);
+        });
+        socket.on('room', (room) => {
+            socket.join(room);
+        });
+        socket.on('emit-room', (room, text) => {
+            io.to(room).emit('message', text);
+        })
+    });
 
-  return io;
-}
+    return io;
+};
